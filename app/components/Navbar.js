9d54e83ffbb8dd093d4ddc46f97448e3312c48fc
vaxad <varadprabhu111@gmail.com>
2023-08-22 20:24:51 +0530

    dark-mode

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -33,29 +33,29 @@ export default function Navbar() {
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
   <div className="block items-center">
       {/* <img src="" className="h-8 mr-3" alt=" Logo" /> */}
-      <span className="self-center lg:text-2xl text-lg font-semibold whitespace-nowrap dark:text-white">NewsDekho2.0</span>
+      <span className="self-center lg:text-2xl text-lg font-semibold whitespace-nowrap text-white">NewsDekho2.0</span>
   </div>
   <div className={`flex ${show?'lg:w-2/3 w-3/5':''} md:order-2 gap-5 transition-all`}>
     {show&&<div className="relative w-full md:block">
       <div className="absolute w-full inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-        <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+        <svg className="w-4 h-4 text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
           <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
         </svg>
         <span className="sr-only">Search icon</span>
       </div>
       <form onSubmit={(e)=>{handleSearch(e)}}>
-      <input value={searchTerm} onChange={(e)=>setSearchTerm(e.target.value)}  type="text" id="search-navbar" className="flex w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..."/>
+      <input value={searchTerm} onChange={(e)=>setSearchTerm(e.target.value)}  type="text" id="search-navbar" className="flex w-full p-2 pl-10 text-sm  border rounded-lg  bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="Search..."/>
       </form>
     </div> }
-    <button onClick={()=>{setshow(!show)}} type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" className=" text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700  rounded-lg text-sm p-2.5 mr-1" >
-      {show?<svg className="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill='currentColor' xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 50 50">
+    <button onClick={()=>{setshow(!show)}} type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" className=" text-gray-400  hover:bg-gray-700  rounded-lg text-sm p-2.5 mr-1" >
+      {show?<svg className="w-5 h-5 text-gray-400" aria-hidden="true" fill='currentColor' xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 50 50">
 <path d="M 14.40625 13 L 13 14.40625 L 23.625 25 L 13 35.59375 L 14.40625 37 L 25.0625 26.40625 L 35.6875 37 L 37.09375 35.59375 L 26.46875 25 L 37.09375 14.40625 L 35.6875 13 L 25.0625 23.59375 Z"></path>
 </svg>:<svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
       </svg>}
       <span className="sr-only">Search</span>
     </button>
-    {!show&&<button onClick={()=>{setshownav(!shownav)}} data-collapse-toggle="navbar-search" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-search" aria-expanded="false">
+    {!show&&<button onClick={()=>{setshownav(!shownav)}} data-collapse-toggle="navbar-search" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg md:hidden  focus:outline-none focus:ring-2 text-gray-400 hover:bg-gray-700 focus:ring-gray-600" aria-controls="navbar-search" aria-expanded="false">
         <span className="sr-only">Open main menu</span>
         <svg className={`w-5 ${shownav?'rotate-90':''} transition-all h-5`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
@@ -65,11 +65,11 @@ export default function Navbar() {
   {!show&&<div className="items-center justify-center hidden w-full md:flex md:w-auto md:order-1" id="navbar-search">
       <div className="relative mt-3 md:hidden">
         <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-          <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+          <svg className="w-4 h-4 text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
           </svg>
         </div>
-        <input type="text" id="search-navbar" className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..."/>
+        <input type="text" id="search-navbar" className="block w-full p-2 pl-10 text-sm  border  rounded-lg  bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="Search..."/>
       </div>
      <ul className="flex flex-col p-4 mx-5 md:p-0 mt-4 font-medium border rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0">
         <li>
--- app/components/NewsItem.js
+++ app/components/NewsItem.js
@@ -8,9 +8,9 @@ export default function NewsItem({item}) {
     </a>
     <div className='flex justify-between items-stretch'>
     <div className="p-5 flex flex-col justify-between">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{item.title}</h5>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{`By ${item.source.name} ${item.time}`}</p>
-        <a href={item.link} target='_blank' className="flex flex-row w-full justify-center items-center  px-3 py-2 text-sm font-medium text-center text-black hover:text-white transition-all bg-slate-700 rounded-lg hover:bg-slate-800 focus:ring-4 focus:outline-none focus:ring-slate-300 dark:bg-slate-100 dark:hover:bg-slate-700 dark:focus:ring-slate-800">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-slate-100">{item.title}</h5>
+        <p className="mb-3 font-normal text-gray-400">{`By ${item.source.name} ${item.time}`}</p>
+        <a href={item.link} target='_blank' className="flex flex-row w-full justify-center items-center  px-3 py-2 text-sm font-medium text-center text-black hover:text-white transition-all  rounded-lg  focus:ring-4 focus:outline-none bg-slate-100 hover:bg-slate-700 focus:ring-slate-800">
             Read more
              <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
